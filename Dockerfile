7fd5b50d4efda3b544680e00fb4caab29de5fcd6
Sergey Ostrovsky <ssostrovskiy@beeline.ru>
2023-11-27 01:59:06 +0300

    очистка проекта перед новым кодом

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.11
-
-COPY . .
-
-CMD ["python3", "/app/main.py"]
--- app/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from API.api import *
-
-if __name__ == "__main__":
-    app.run()
